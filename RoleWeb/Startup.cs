a84acf072c80e194eeb8567c444047fccd4940bb
Brad Merrill <brad_merrill@hotmail.com>
2015-03-19 09:06:35 -0700

    apply local refactoring

---
+++ RoleWeb/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+using R = RolesLib;
+
+namespace RoleWeb
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            R.Startup.ConfigureAuth(app, Properties.Settings.Default.LoginPath);
+        }
+    }
+}
